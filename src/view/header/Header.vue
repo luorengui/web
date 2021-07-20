3fda55a7690bb09ee475e1a818d12b2b300a526a
Luorengui <admin@DESKTOP-0HJLQNA>
2021-07-20 16:07:27 +0800

    feat:修改界面

--- src/view/header/Header.vue
+++ src/view/header/Header.vue
@@ -1,45 +1,16 @@
 <template>
-  <!-- <v-container> -->
     <v-app-bar app color="white">
       <v-app-bar-nav-icon @click="showNav"></v-app-bar-nav-icon>
       <v-toolbar-title>hello</v-toolbar-title>
-      <template v-slot:extension>
-        <v-tabs centered>
-          <v-tab
-            v-model="tab"
-            v-for="item in tab"
-            :key="item.name"
-            @click="selectTab(item.name)"
-            
-          >
-            {{ item.value }}
-          </v-tab>
-        </v-tabs>
-      </template>
     </v-app-bar>
-  <!-- </v-container> -->
 </template>
 
 <script>
 export default {
   name: "Header",
-  // props:["drawer"],
   data: () => ({
     drawer: false,
-    tab: [
-      {
-        name: "Blog",
-        value: "Blog",
-      },
-      {
-        name: "About",
-        value: "About",
-      },
-      {
-        name: "Skill",
-        value: "Skill",
-      },
-    ],
+   
   }),
   methods: {
     selectTab(name) {
--- src/view/main/Main.vue
+++ src/view/main/Main.vue
@@ -1,23 +1,41 @@
 <template>
-    <v-main>
-      
-      <v-container fluid>
-        {{selectTab}}
-      <!-- If using vue-router -->
-      <router-view></router-view>
-    </v-container>
-
-    </v-main>
+  <v-main>
+      <v-tabs centered>
+        <v-tab
+          v-model="tab"
+          v-for="item in tab"
+          :key="item.name"
+          @click="selectTab(item.name)"
+        >
+          {{ item.value }}
+        </v-tab>
+      </v-tabs>
+  </v-main>
 </template>
 
 <script>
 export default {
   name: "Main",
-  props:["selectTab"],
-  created(){
-    console.log(this.selectTab,"www");  
+  props: ["selectTab"],
+  created() {
+    console.log(this.selectTab, "www");
   },
-  data: () => ({}),
+  data: () => ({
+     tab: [
+      {
+        name: "Blog",
+        value: "Blog",
+      },
+      {
+        name: "About",
+        value: "About",
+      },
+      {
+        name: "Skill",
+        value: "Skill",
+      },
+    ],
+  }),
   methods: {},
 };
 </script>
